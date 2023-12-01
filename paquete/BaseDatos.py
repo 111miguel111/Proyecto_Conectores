26431b96f88245ecb0edbea724554878ae544050
111miguel111 <132752164+111miguel111@users.noreply.github.com>
2023-12-01 18:59:01 +0100

    baseDatos1

--- paquete/BaseDatos.py
+++ paquete/BaseDatos.py
@@ -20,15 +20,74 @@ def mysqlconnect():
         )
     
     return conn
+def iniciar():
+    conn =mysqlconnect()
+    cur = conn.cursor()
+    cur.execute('select @@version')
+    cur.execute('''CREATE DATABASE IF NOT EXISTS Miguel_Roberto ;''')
+    
+    cur.execute('''CREATE TABLE IF NOT EXISTS alumno_curso
+            (
+            id_curso int NOT NULL PRIMARY KEY ,
+            id_alumno int NOT NULL PRIMARY KEY
+            );
+            ''')
+             
+    cur.execute('''CREATE TABLE IF NOT EXISTS profesores
+            (
+            id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+            dni VARCHAR(9) UNIQUE NOT NULL,
+            nombre VARCHAR(25) NOT NULL,
+            direccion VARCHAR(25) NOT NULL
+            );
+            ''')
+    cur.execute('''CREATE TABLE IF NOT EXISTS cursos
+            (
+            id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+            nombre VARCHAR(25) UNIQUE NOT NULL,
+            descripcion VARCHAR(25) NOT NULL,
+            id_profesor int ,
+            id_alumno_curso int,
+            FOREIGN KEY (id_profesor) REFERENCES profesores (id)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE,
+            FOREIGN KEY (id_alumno_curso) REFERENCES alumno_curso (id_curso)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE
+            );
+            ''')
+    cur.execute('''CREATE TABLE IF NOT EXISTS alumnos
+            (
+            id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+            nombre VARCHAR(25) UNIQUE NOT NULL,
+            apellidos VARCHAR(25) NOT NULL,
+            telefono VARCHAR(25) NOT NULL,
+            direccion VARCHAR(25) NOT NULL,
+            f_nacimiento VARCHAR(25) NOT NULL,
+            id_alumno_curso int,
+            FOREIGN KEY (id_alumno_curso) REFERENCES alumno_curso(id_alumno)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE
+            );
+            ''')
+    cur.execute('''ALTER TABLE alumno_curso 
+            (
+            FOREIGN KEY (id_alumno) REFERENCES alumnos (id)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE,
+            FOREIGN KEY (id_curso) REFERENCES cursos (id)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE
+                
+            );
+            ''')
+    conn.commit()
+    return 0
 conn =mysqlconnect()
 cur = conn.cursor()
-cur.execute('select @@version')
-cur.execute('''CREATE DATABSE IF NOT EXISTS Miguel_Roberto ;''')
-cur.execute('''CREATE TABLE IF NOT EXISTS profesores
-         (ID int NOT NULL PRIMARY KEY,
-         Dni VARCHAR(9) NOT NULL,
-         Nombre VARCHAR(25) NOT NULL,
-         Direccion VARCHAR(25) NOT NULL,
-         );
-
 cur.execute('USE Miguel_Roberto')
+
+
+
+
+
